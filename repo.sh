cd44b2eb1be332e5b81615168223bb692e14978e
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-06-30 12:57:11 -0300

    1
    1

--- repo.sh
+++ repo.sh
@@ -3,7 +3,6 @@ mkdir ~/server/11/sources
 cd ~/server/11/
 sh repo-adhoc.sh
 sh repo-oca.sh
-sh repo-it-labs.sh
 cd ~/server/11/sources
 find . -name "oca_dependencies.txt" -type f -delete
 cd ~/server/11
