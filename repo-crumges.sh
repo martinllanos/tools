9c3de8dd33242741c238c0abdf2730f6340a1b81
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-07-18 16:42:17 -0300

    1
    1

---
+++ repo-crumges.sh
@@ -0,0 +1,9 @@
+cd ~/server/12/sources
+git clone --depth 1 --branch 12.0 --single-branch https://github.com/marionumza/addons.git crumges-addons
+#cd ~/server/12/sources
+#find . -name "oca_dependencies.txt" -type f -delete
+#cd ~/server/12/
+#sources bin/activate
+#cd ~/server/12
+#./maintainer-quality-tools/travis/clone_oca_dependencies sources/ .
+#find ~/server/12/sources -mindepth 1 -maxdepth 1 -type d | sort | tr '\n' ','
