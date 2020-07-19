246292bd628088edfcf67d9f6a3addb7c0298670
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-07-19 15:55:50 -0300

    1
    1

--- repo-oca.sh
+++ repo-oca.sh
@@ -5,17 +5,17 @@ git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/account
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/account-financial-reporting.git OCA-account-financial-reporting
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/account-financial-tools.git OCA-account-financial-tools
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/account-payment.git OCA-account-payment
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/apps-store.git OCA-apps-store
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/apps-store.git OCA-apps-store
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/bank-payment.git OCA-bank-payment
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/calendar.git OCA-calendar
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/commission.git OCA-commission
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/calendar.git OCA-calendar
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/commission.git OCA-commission
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/credit-control.git OCA-credit-control
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/crm.git OCA-crm
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/currency.git OCA-currency
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/ddmrp.git OCA-ddmrp
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/delivery-carrier.git OCA-delivery-carrier
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/e-commerce.git OCA-e-commerce
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/event.git OCA-event
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/event.git OCA-event
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/field-service.git OCA-field-service
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/geospatial.git OCA-geospatial
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/hr-timesheet.git OCA-hr-timesheet
@@ -30,11 +30,10 @@ git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/oca-cus
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/OpenUpgrade.git OCA-OpenUpgrade
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/operating-unit.git OCA-operating-unit
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/partner-contact.git OCA-partner-contact
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/pos.git OCA-pos
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/pos.git OCA-pos
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/product-attribute.git OCA-product-attribute
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/product-variant.git OCA-product-variant
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/product-variant.git OCA-product-variant
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/project-reporting.git OCA-project-reporting
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/project-service.git OCA-project-service
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/project.git OCA-project
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/purchase-workflow.git OCA-purchase-workflow
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/queue.git OCA-queue
@@ -55,9 +54,9 @@ git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/stock-l
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/timesheet.git OCA-timesheet
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/vertical-association.git OCA-vertical-association
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/web.git OCA-web
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/website-themes.git OCA-website-themes
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/website-themes.git OCA-website-themes
 git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/website.git OCA-website
-git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/contract.git OCA-contract
+#git clone --depth 1 --branch 13.0 --single-branch https://github.com/OCA/contract.git OCA-contract
 #cd ~/server/13/sources
 #find . -name "oca_dependencies.txt" -type f -delete
 #cd ~/server/13/
