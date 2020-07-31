9b4f91c56750fb4c307b498ca76b4694d21150ff
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-07-31 14:59:39 -0300

    1
    1

--- repo-oca-account.sh
+++
@@ -1,7 +0,0 @@
-cd ~/server/12/sources
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-analytic.git OCA-account-analytic
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-closing.git OCA-account-closing
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-consolidation.git OCA-account-consolidation
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-financial-reporting.git OCA-account-financial-reporting
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-financial-tools.git OCA-account-financial-tools
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/account-payment.git OCA-account-payment
--- repo-oca-product.sh
+++
@@ -1,4 +0,0 @@
-cd ~/server/12/sources
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/product-attribute.git OCA-product-attribute
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/product-pack.git OCA-product-pack
-git clone --depth 1 --branch 12.0 --single-branch https://github.com/OCA/product-variant.git OCA-product-variant
